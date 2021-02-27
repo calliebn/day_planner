c2d76cadd97271d5df23918b67420367ef68f20a
Callie Nipper <callie.nipper@gmail.com>
2021-02-27 15:01:53 -0800

    Cleaned up JS file

--- js/script.js
+++ js/script.js
@@ -67,30 +67,15 @@ $(".time-block").each(function () {
 
 // Saves the tasks to the planner
 $(".saveBtn").on("click", function () {
-    //event.preventDefault()
-    //let id = JSON.stringify(timeNew)
     let hour = $(this).siblings(".hour").children().first().text()
     let taskarea = $(this).siblings("textarea").val()
     let task = localStorage.setItem(hour, taskarea)
-    //let task = localstorage.setItem("taskarea")
     localStorage.getItem(task)
-    //let task = $(this).siblings(".task").val()
 
-    //grab the block hour
-    //set local storage item (hour, task)
 })
+
 // Clears all events from the planner
 $(".clear").on("click", function () {
     localStorage.clear()
     $(".time-block").find("textarea").val("");
 }) 
-
-// On load, the page should check the current date and time
-// The date in the header should reflect the current date
-// IF the time is in the past, the past class should apply itself to the time-block
-// IF the time is in the current hour, the present class should apply itself to the time-block
-// IF the time is in the future, the future class should apply itself to the time-block
-
-// When the user clicks in the text area for each time-block, they should be able to enter events
-// When the user clicks the save icon, the event should be saved to their local storage
-// When the page is refreshed, the events should not disappear unless it is a new day?
